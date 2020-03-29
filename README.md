7a6e5c5cd2e6ee8309cc84300c25b983364baf87
Paulo Sergio Poli Junior <psergiopoli@gmail.com>
2020-03-29 01:27:10 -0300

    Hello World bot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-#pbot
-#A discord bot
+# pbot
+## A discord bot
+
+## invite url: https://discordapp.com/api/oauth2/authorize?client_id=693628612087250945&permissions=3221504&scope=bot
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World")
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+client.on('message', (msg: { content: string; reply: (arg: string) => void; }) => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+ 
+const token = process.env.DISCORD_TOKEN;
+console.log(`token: ${token}`);
+client.login(token);
