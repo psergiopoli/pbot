e196658f1a99d83522e58c818b203d06ce8c0ea8
Paulo Sergio Poli Junior <psergiopoli@gmail.com>
2020-04-14 17:04:51 -0300

    versao 0.5

--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
-docker build . --tag pbot-0.4
-docker tag e1daff2c8d98 psergiopoli/pbot:0.4
-docker push psergiopoli/pbot:0.4
+docker build . --tag pbot-0.5
+docker tag adc111fdb2ec psergiopoli/pbot:0.5
+docker push psergiopoli/pbot:0.5
